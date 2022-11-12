47b1969e788b1b57aa98cfee04915805e738d6a3
lostincn <aidaroffski1@gmail.com>
2022-11-13 01:15:53 +0600

    created Hooks useTelegram

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const { tg, onToggleButton } = useTelegram();
+  useEffect(() => {
+    tg.ready();
+  }, []);
   return (
     <div className="App">
       <h1>Hello this is website for bot</h1>
-      <button>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Button from "../Button/Button";
 import { useEffect } from "react";
+import { useTelegram } from "../hooks/useTelegram";
+
 const Header = () => {
-  const tg = window.Telegram.WebApp;
-  useEffect(() => {
-    tg.ready();
-  }, []);
-  const onClose = () => {
-    tg.close();
-  };
+  const { user, onClose } = useTelegram();
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+      <span className={"username"}>{user.username}</span>
     </div>
   );
 };
