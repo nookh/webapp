452e4a90301396a49eec19b5f01203756102b293
lostincn <aidaroffski1@gmail.com>
2022-11-13 01:32:12 +0600

    'header'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useTelegram } from "../hooks/useTelegram";
 
 const Header = () => {
