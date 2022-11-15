c81a617d3804cc96e3126516d9f6069bf878d809
lostincn <aidaroffski1@gmail.com>
2022-11-15 23:34:10 +0600

    bug

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -27,7 +27,7 @@ const Form = () => {
     return () => {
       tg.offEvent("mainButtonClicked", onSendData);
     };
-  }, []);
+  }, [onSendData]);
   useEffect(() => {
     if (!country || !city) {
       tg.MainButton.hide();
